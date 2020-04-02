11d89ce0c6c053aa65a343d725fcc274ca302657
Yaroslav Pylaev <ypylaev@icloud.com>
2020-04-02 16:08:19 +0300

    First synchronized functions

---
+++ src/com/labs/l3/examples/tourniquet/Counter.java
@@ -0,0 +1,36 @@
+package com.labs.l3.examples.tourniquet;
+
+public class Counter {
+    private int counter;
+    static public int iExample;
+
+    public void inc() {
+        //critical section
+        synchronized (this) { //mutex = mutual exclusion
+            ++counter;
+            SynchronizedTourniquet.printInfo();
+        }
+    }
+    //==
+    synchronized public void inc2() {
+        synchronized (this) {
+            counter *= 2;
+            SynchronizedTourniquet.printInfo();
+        }
+    }
+
+    public int get() {
+        return counter;
+    }
+
+    //---Example of synchronized static
+    synchronized static public void incExample() {
+        ++iExample;
+    }
+    //==
+    static public void incExample2() {
+        synchronized (SynchronizedTourniquet.class) {
+            ++iExample;
+        }
+    }
+}
